631cb7d3a4d52ccae2861ae67db6022e76da2da9
Hondunauta <klauswiese@users.noreply.github.com>
2021-07-06 08:02:53 -0600

    Update PlanetScopeNICFI.R
    Update to june 2021

--- R/PlanetScopeNICFI.R
+++ R/PlanetScopeNICFI.R
@@ -21,7 +21,8 @@ PlanetScopeNICFI <- function(){
                                                                             "February 2021",
                                                                             "March 2021",
                                                                             "April 2021",
-                                                                            "May 2021"))
+                                                                            "May 2021",
+                                                                            "June 2021"))
   
   l <- list(NICFI_BI, NICFI_Monthly)
   names(l) <- c("Biannual Collection", "Monthly Collection")
